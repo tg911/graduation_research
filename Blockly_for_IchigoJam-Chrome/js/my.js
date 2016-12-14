5c44a65dafec6fb5ff7729a10df08d9ec0c2b436
tg <e1q13031@oit.ac.jp>
2016-12-14 19:38:44 +0900

    Blockly for IchigoJamをChromeApp化
    IchigoJamBASIC以外の言語タブを非表示に。
    実行ボタンを非表示に。
    ブロック全消去ボタンが効かない不具合要修正。

---
+++ Blockly_for_IchigoJam-Chrome/js/my.js
@@ -0,0 +1,44 @@
+document.getElementById('languageMenu').setAttribute("style", "display:none");
+var container = document.getElementById('content_area');
+var margin_off = function(e) {
+var bBox = Code.getBBox_(container);
+for (var i = 0; i < Code.TABS_.length; i++) {
+   var el = document.getElementById('content_' + Code.TABS_[i]);
+   el.style.top = bBox.y + 'px';
+   el.style.left = bBox.x + 'px';
+   // Height and width need to be set, read back, then set again to
+   // compensate for scrollbars.
+   el.style.height = bBox.height + 'px';
+   el.style.height = (2 * bBox.height - el.offsetHeight) + 'px';
+   el.style.width = bBox.width + 'px';
+   el.style.width = (2 * bBox.width - el.offsetWidth) + 'px';
+   }
+   // Make the 'Blocks' tab line up with the toolbox.
+   if (Code.workspace && Code.workspace.toolbox_.width) {
+   document.getElementById('tab_blocks').style.minWidth =
+   (Code.workspace.toolbox_.width - 38) + 'px';
+   // Account for the 19 pixel margin and on each side.
+   }
+};
+// 謎マージンを消す
+(window.onload = function() {
+ margin_off();
+})();
+
+// value input fieldsの括弧を消す
+// var brackets_off = function(e) {
+// 予定
+// };
+
+// ブロックの彩度を変更
+// Blockly.HSV_SATURATION = 1.00;
+// Blockly.HSV_VALUE = 1.00;
+
+// IchigoJamBASIC以外の言語と実行ボタンを非表示に
+document.getElementById('runButton').setAttribute("style", "display:none");
+document.getElementById('tab_javascript').setAttribute("style", "display:none");
+document.getElementById('tab_python').setAttribute("style", "display:none");
+document.getElementById('tab_php').setAttribute("style", "display:none");
+document.getElementById('tab_lua').setAttribute("style", "display:none");
+document.getElementById('tab_dart').setAttribute("style", "display:none");
+document.getElementById('tab_xml').setAttribute("style", "display:none");
---
+++ Blockly_for_IchigoJam-Chrome/msg/ja.js
@@ -0,0 +1,22 @@
+var MSG = {
+  title: "コード",
+  blocks: "ブロック",
+  linkTooltip: "ブロックの状態を保存してリンクを取得します。",
+  runTooltip: "ブロックで作成したプログラムを実行します。",
+  badCode: "プログラムのエラー:\n%1",
+  timeout: "命令の実行回数が制限値を超えました。",
+  trashTooltip: "すべてのブロックを消します。",
+  catOutput: "出力",
+  catInput: "入力",
+  catLoops: "繰り返し",
+  catVariables: "条件分岐",
+  catCalculations: "計算",
+  catNotes: "音符",
+  catProcedures: "関数",
+  catOthers: "その他",
+  httpRequestError: "ネットワーク接続のエラーです。",
+  linkAlert: "ブロックの状態をこのリンクで共有できます:\n\n%1",
+  hashError: "すみません。「%1」という名前のプログラムは保存されていません。",
+  xmlError: "保存されたファイルを読み込めませんでした。別のバージョンのブロックリーで作成された可能性があります。",
+  badXml: "XML のエラーです:\n%1\n\nXML の変更をやめるには「OK」、編集を続けるには「キャンセル」を選んでください。"
+};
