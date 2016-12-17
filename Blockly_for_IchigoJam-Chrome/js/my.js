0594b84a17570af9b4a870d60cdcd8c6311249a1
tg <e1q13031@oit.ac.jp>
2016-12-17 21:26:22 +0900

    BTNブロック修正&ポートセレクタ追加
    Blockly for IchigoJamのbtn.jsから出力されるコードから改行を削除。
    ChromeApps版にも反映。
    ポートセレクタを追加。

--- Blockly_for_IchigoJam-Chrome/js/my.js
+++ Blockly_for_IchigoJam-Chrome/js/my.js
@@ -1,34 +1,40 @@
 document.getElementById('languageMenu').setAttribute("style", "display:none");
 var container = document.getElementById('content_area');
 var margin_off = function(e) {
-var bBox = Code.getBBox_(container);
-for (var i = 0; i < Code.TABS_.length; i++) {
-   var el = document.getElementById('content_' + Code.TABS_[i]);
-   el.style.top = bBox.y + 'px';
-   el.style.left = bBox.x + 'px';
-   // Height and width need to be set, read back, then set again to
-   // compensate for scrollbars.
-   el.style.height = bBox.height + 'px';
-   el.style.height = (2 * bBox.height - el.offsetHeight) + 'px';
-   el.style.width = bBox.width + 'px';
-   el.style.width = (2 * bBox.width - el.offsetWidth) + 'px';
-   }
-   // Make the 'Blocks' tab line up with the toolbox.
-   if (Code.workspace && Code.workspace.toolbox_.width) {
-   document.getElementById('tab_blocks').style.minWidth =
-   (Code.workspace.toolbox_.width - 38) + 'px';
-   // Account for the 19 pixel margin and on each side.
-   }
+  var bBox = Code.getBBox_(container);
+  for (var i = 0; i < Code.TABS_.length; i++) {
+    var el = document.getElementById('content_' + Code.TABS_[i]);
+    el.style.top = bBox.y + 'px';
+    el.style.left = bBox.x + 'px';
+    // Height and width need to be set, read back, then set again to
+    // compensate for scrollbars.
+    el.style.height = bBox.height + 'px';
+    el.style.height = (2 * bBox.height - el.offsetHeight) + 'px';
+    el.style.width = bBox.width + 'px';
+    el.style.width = (2 * bBox.width - el.offsetWidth) + 'px';
+  }
+  // Make the 'Blocks' tab line up with the toolbox.
+  if (Code.workspace && Code.workspace.toolbox_.width) {
+    document.getElementById('tab_blocks').style.minWidth =
+    (Code.workspace.toolbox_.width - 38) + 'px';
+    // Account for the 19 pixel margin and on each side.
+  }
 };
-// 謎マージンを消す
-(window.onload = function() {
- margin_off();
-})();
 
-// value input fieldsの括弧を消す
-// var brackets_off = function(e) {
-// 予定
-// };
+window.onload = function() {
+  margin_off();  // 謎マージンを消す
+  chrome.serial.getDevices(function(ports) {
+    console.log(ports.length);
+    var portsel = document.getElementById("portsel");
+    for (var i = ports.length - 1; i >= 0; i--) {
+      var port = ports[i];
+      console.log(port.path);
+      var opt = document.createElement("option");
+      opt.value = opt.innerText = port.path;
+      portsel.appendChild(opt);
+    }
+  });
+};
 
 // ブロックの彩度を変更
 // Blockly.HSV_SATURATION = 1.00;
