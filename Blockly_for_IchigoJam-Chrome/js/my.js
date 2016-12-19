3fdd5b8187d0e0a0b6df3b1e64de2cf919ca247e
tg <e1q13031@oit.ac.jp>
2016-12-19 21:06:55 +0900

    生成したコードをIchigoJamへ書き込めるように
    ポート接続ボタン追加。
    ポートとの接続状態を確認できるように。
    生成したコードをIchigoJamへ書き込むボタンを追加。
    行番号を付ける処理を追加。
    コードの取得や文字コードへの変換は上手くいっているはずだが、何故かコードをIchigoJamへ送信した時にコードの1行目までしか表示されない。

--- Blockly_for_IchigoJam-Chrome/js/my.js
+++ Blockly_for_IchigoJam-Chrome/js/my.js
@@ -21,6 +21,140 @@ var margin_off = function(e) {
   }
 };
 
+var setStatus = function(s) {
+  document.getElementById("status").innerText = s;
+  document.getElementById("btn").innerText = s == "connected" ? "disconnect" : "connect";
+};
+
+var conid = -1;
+var t = null;
+
+// var setLineNumber = function(e) {
+//   var contentsTag = document.getElementById("content_ichigojambasic").getElementsByTagName("span");
+//   var code = "";
+//   // htmlのタグの中身を取り出す
+//   var getContents = function(e) {
+//     for (var i = 0; i < contentsTag.length; i++){
+//       code += contentsTag[i].innerText;
+//     }
+//   };
+//   getContents();
+//
+//   var newLineCharNum = (code.match(/\n/g)||[]).length;
+//
+//   var contentsTag2 = document.getElementById("content_ichigojambasic");
+//   var lineNum = 10;
+//
+//   for (var i = 0; i <= newLineCharNum; i++){
+//     var spanNode = document.createElement('span');
+//     var textNode = document.createTextNode(lineNum + " ");
+//     spanNode.appendChild(textNode);
+//     if(i == 0) ? contentsTag2.insertBefore(spanNode,contentsTag2.firstChild); :
+//     contentsTag2.appendChild(spanNode);
+//     lineNum += 10;
+//   }
+// };
+
+// var setLineNumber = function(e) {
+//   var contentsTag = document.getElementById("content_ichigojambasic").getElementsByTagName("span");
+//   var lineNum = 10;
+//   for (var i = 0; i < contentsTag.length; i++){
+//     if (i == 0){
+//       contentsTag[i].innerText = lineNum + " " + contentsTag[i].innerText;
+//       lineNum += 10;
+//     }
+//     console.log("i:" + i)
+//     if ((contentsTag[i].innerText.match(/\r/g)||[]).length != 0){
+//
+//       console.log("contentsTag[" + i + "]:" + contentsTag[i].innerText)
+//       // contentsTag[i-1].innerText = contentsTag[i-1].innerText + "\n";
+//       // contentsTag[i].innerText = (lineNum + " " + contentsTag[i].innerText).replace(/\n/,"");
+//       contentsTag[i].innerText = lineNum + " " + contentsTag[i].innerText;
+//       // contentsTag[i].insertBefore(lineNum, contentsTag[i].firstChild)
+//       lineNum += 10;
+//     }
+//   }
+// };
+
+var setLineNumber = function(e) {
+  var contentsTag = document.getElementById("content_ichigojambasic").getElementsByTagName("span");
+  var lineNum = 10;
+  var code = "";
+  for (var i = 0; i < contentsTag.length; i++){
+    code += contentsTag[i].innerText;
+  }
+  // console.log(code)
+  var sliceCode = code.split('');
+  // console.log(sliceCode)
+  // console.log(sliceCode.length)
+  for (var i = 0; i < sliceCode.length; i++) {
+    if (i == 0) {
+      sliceCode.splice(i, 0, lineNum, " ");
+      sliceCode.length += 2;
+      lineNum += 10;
+    }
+    if (sliceCode[i] == "\n") {
+      sliceCode.splice(i+1, 0, lineNum, " ");
+      sliceCode.length += 2;
+      lineNum += 10;
+    }
+  }
+  code = sliceCode.join("");
+  console.log(sliceCode)
+  // console.log("finalcode:" + code);
+
+  document.getElementById("content_ichigojambasic").innerText = code;
+};
+
+var sendToIchigoJam = function(e) {
+  console.log("sendToIchigoJam")
+  if (conid == -1)
+  return;
+  // var contentsTag = document.getElementById("content_ichigojambasic").getElementsByTagName("span");
+  // var code = "";
+  // htmlのタグの中身を取り出す
+  // var getContents = function(e) {
+  //   for (var i = 0; i < contentsTag.length; i++){
+  //     code += contentsTag[i].innerText;
+  //   }
+  // };
+  // getContents();
+
+  var contentsTag = document.getElementById("content_ichigojambasic");
+  var sliceCode = contentsTag.innerText.split('');
+  // console.log(code)
+  // console.log(code.length)
+  var buffer = new ArrayBuffer(sliceCode.length);
+  // console.log("buffer:" + buffer)
+  var view = new Uint8Array(buffer);
+  // console.log("viewBefore:" + view)
+  console.log(sliceCode)
+  // 文字をキャラクターコードに変換
+  for (var i = 0; i < sliceCode.length; i++) {
+    view[i] = sliceCode[i].charCodeAt(0);
+  }
+  console.log("viewAfter:" + view)
+  chrome.serial.send(conid, buffer, function(info) {
+    console.log("send: " + info.bytesSent);
+    chrome.serial.flush(conid, function(res) {
+      console.log("flush: " + res);
+    });
+  });
+};
+
+var open = function(port) {
+  chrome.serial.connect(port, { bitrate: 115200 }, function(info) {
+    //	console.log("info: " + info);
+    conid = info.connectionId;
+    console.log("connectionId: " + conid);
+    if (conid == -1) {
+      setStatus("connect err");
+      return;
+    }
+    setStatus("connected");
+  });
+};
+
 window.onload = function() {
   margin_off();  // 謎マージンを消す
   chrome.serial.getDevices(function(ports) {
@@ -33,7 +167,22 @@ window.onload = function() {
       opt.value = opt.innerText = port.path;
       portsel.appendChild(opt);
     }
+    var connect = function() {
+      if (conid != -1) {
+        chrome.serial.disconnect(conid, function(res) {
+          console.log("disconnec: " + res);
+          setStatus("Idle");
+        });
+        conid = -1;
+        return;
+      }
+      open(portsel.value);
+    };
+    portsel.onchange = connect;
+    document.getElementById("btn").onclick = connect;
   });
+  document.getElementById("send").onclick = sendToIchigoJam;
+  document.getElementById("tab_ichigojambasic").onclick = setLineNumber;
 };
 
 // ブロックの彩度を変更
